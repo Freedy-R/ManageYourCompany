0d7a112ce1475745ab285ca36b493417044ba432
Freedy-R <dedspinpolski@gmail.com>
2022-05-05 15:55:34 +0200

    Add project files.

---
+++ Biznes Menedżer/.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace Biznes_Menedżer
+{
+    partial class fStan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(263, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ilość do zmienienia Stanu:\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(12, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Zmiana Stanu";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numericUpDown1.Location = new System.Drawing.Point(17, 61);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(258, 31);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(17, 172);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(258, 33);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(17, 298);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 73);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Anuluj";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(177, 298);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 73);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Zmień";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // fStan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(313, 383);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "fStan";
+            this.Text = "Zmień Stan";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Biznes Menedżer/Menu.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace Biznes_Menedżer
+{
+    partial class fMenu
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabMenu = new System.Windows.Forms.TabControl();
+            this.tabpMenu = new System.Windows.Forms.TabPage();
+            this.tabObiekty = new System.Windows.Forms.TabPage();
+            this.tabMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabMenu
+            // 
+            this.tabMenu.Controls.Add(this.tabpMenu);
+            this.tabMenu.Controls.Add(this.tabObiekty);
+            this.tabMenu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabMenu.HotTrack = true;
+            this.tabMenu.Location = new System.Drawing.Point(0, 0);
+            this.tabMenu.Name = "tabMenu";
+            this.tabMenu.SelectedIndex = 0;
+            this.tabMenu.Size = new System.Drawing.Size(1368, 707);
+            this.tabMenu.TabIndex = 0;
+            // 
+            // tabpMenu
+            // 
+            this.tabpMenu.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.tabpMenu.Location = new System.Drawing.Point(4, 22);
+            this.tabpMenu.Name = "tabpMenu";
+            this.tabpMenu.Padding = new System.Windows.Forms.Padding(3);
+            this.tabpMenu.Size = new System.Drawing.Size(1360, 681);
+            this.tabpMenu.TabIndex = 0;
+            this.tabpMenu.Text = "Menu";
+            // 
+            // tabObiekty
+            // 
+            this.tabObiekty.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.tabObiekty.Location = new System.Drawing.Point(4, 22);
+            this.tabObiekty.Name = "tabObiekty";
+            this.tabObiekty.Padding = new System.Windows.Forms.Padding(3);
+            this.tabObiekty.Size = new System.Drawing.Size(1360, 681);
+            this.tabObiekty.TabIndex = 1;
+            this.tabObiekty.Text = "Obiekty";
+            // 
+            // fMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1368, 707);
+            this.Controls.Add(this.tabMenu);
+            this.Name = "fMenu";
+            this.Text = "Biznes Menedżer";
+            this.tabMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabMenu;
+        private System.Windows.Forms.TabPage tabpMenu;
+        private System.Windows.Forms.TabPage tabObiekty;
+    }
+}
+
